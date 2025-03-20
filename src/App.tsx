d3b86c9ffd5b36eb7881affbccef1c3abece33b3
Kotaro <kotaro@KotaronoMacBook-Air.local>
2025-03-20 21:59:42 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { css } from '../styled-system/css';
+
+function App() {
+
+  return (
+    <div className={css({ display: "flex", alignItems: "center", justifyContent: "center", minHeight: "100%", fontSize: "2xl", fontWeight: 'bold', textAlign: "center" })}>Hello 🐼!</div>
+  )
+}
+
+export default App
