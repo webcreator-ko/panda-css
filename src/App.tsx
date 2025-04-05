4c800c99b46a5b3c366819341ad00c31918bf39c
Kotaro <kotaro@KotaronoMacBook-Air.local>
2025-04-05 16:35:21 +0900

    headerタグなどの使用方法

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Box } from '@styled-system/jsx'
+import { Box, styled } from '@styled-system/jsx'
+const Header = styled('header')
+
 function App() {
 
   return (
-    <>
-    {/* 2つのカラーコード */}
-    <Box alignItems="center" color="red" >Hello 🐼!</Box>
-    <Box alignItems="center"  color="var(--colors-red)">Hello 🐼!</Box>
-    </>
+    <Header bg="gold">
+      {/* 2つのカラーコード */}
+      <Box alignItems="center" color="red" >Hello 🐼!</Box>
+      <Box alignItems="center"  color="var(--colors-red)">Hello 🐼!</Box>
+    </Header>
   )
 }
 
